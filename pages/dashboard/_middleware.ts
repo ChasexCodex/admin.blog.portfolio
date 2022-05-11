dda8cc4cb61f6b83f22c2fcd523242ee0c451ea5
ElyasAmri <elyas.amri@gmail.com>
2022-05-11 22:03:16 +0300

    chore: change middleware

--- pages/dashboard/_middleware.ts
+++ pages/dashboard/_middleware.ts
@@ -2,15 +2,10 @@ import {NextRequest, NextResponse} from 'next/server'
 import {checkUser} from '../../utils/auth'
 
 export async function middleware(req: NextRequest) {
-  const authHeader = req.headers.get('Authorization')
+  const header = req.headers.get("Authorization")
 
-  if (authHeader) {
-    const match = authHeader.match(/Bearer (.*)/)
-    const token = match ? match[1] : null
-
-    if(checkUser(token)) {
-      return NextResponse.next()
-    }
+  if (checkUser(header)) {
+    return NextResponse.next()
   }
 
   return new Response('Auth required', {
