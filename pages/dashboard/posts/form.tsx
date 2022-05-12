08736edd8801c8f2a831784e5ab6d39c3f2dcc69
ElyasAmri <elyas.amri@gmail.com>
2022-05-12 13:22:56 +0300

    fix: add post form content

--- pages/dashboard/posts/form.tsx
+++ pages/dashboard/posts/form.tsx
@@ -1,7 +1,35 @@
+import {useState} from 'react'
+
+
 const FormPost = () => {
+
+  const [title, setTitle] = useState('The Second Post')
+  const [slug, setSlug] = useState('the-second-post')
+  const [author, setAuthor] = useState('Elyas A. Al-Amri')
+  const [content, setContent] = useState('This post was made using my editor.')
+
+  const changeTitle = (e: any) => setTitle(e.target.value)
+  const changeSlug = (e: any) => setSlug(e.target.value)
+  const changeAuthor = (e: any) => setAuthor(e.target.value)
+  const changeContent = (e: any) => setContent(e.target.value)
+
   return (
       <div>
-        Post Form
+        <form>
+          <label htmlFor="title">Title</label>
+          <input value={title} onChange={changeTitle} type="text" name="title" required/>
+
+          <label htmlFor="slug">Slug</label>
+          <input value={slug} onChange={changeSlug} type="text" name="slug" placeholder="Auto-Generated"/>
+
+          <label htmlFor="content">Content</label>
+          <textarea value={content} onChange={changeContent} name="content" required/>
+
+          <label htmlFor="author">Author</label>
+          <input value={author} onChange={changeAuthor} type="text" name="author" required/>
+
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
   )
 }
