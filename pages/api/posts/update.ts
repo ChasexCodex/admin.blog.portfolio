6795a4a2f0649aeca7587a63aab9ef829b5dee53
ElyasAmri <elyas.amri@gmail.com>
2022-05-12 11:45:48 +0300

    chore: add update post api

---
+++ pages/api/posts/update.ts
@@ -0,0 +1,30 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import {supabase} from '../../../utils/supabase'
+import Joi from 'joi'
+
+const UpdatePostSchema = Joi
+    .object({
+      id: Joi.string().required(),
+      title: Joi.string().min(3).max(50),
+      slug: Joi.string(),
+      content: Joi.string(),
+      author: Joi.string(),
+    })
+
+export default async function UpdatePost(req: NextApiRequest, res: NextApiResponse) {
+  const {value, error} = UpdatePostSchema.validate(req.body)
+
+  if (error) {
+    return res.status(400).json({error})
+  }
+
+  const updated_at = new Date().toString()
+
+  const result = await supabase.from('posts').update({...value, updated_at})
+
+  if (result.error) {
+    return res.status(400).json({result, updated: false})
+  }
+
+  return res.status(201).json({updated: true, result})
+}
