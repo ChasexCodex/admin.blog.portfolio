a8dbda3d6bf3604c67f0698d71d67c8a0a979e3e
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:24:11 +0300

    chore: add category store api

---
+++ pages/api/categories/store.ts
@@ -0,0 +1,24 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import Joi from 'joi'
+import prisma from '../../../utils/prisma'
+
+const StoreCategorySchema = Joi
+    .object({
+      name: Joi.string().required(),
+    })
+
+export default async function StorePost(req: NextApiRequest, res: NextApiResponse) {
+  const {value, error} = StoreCategorySchema.validate(req.body)
+
+  if (error) {
+    res.status(400).json({error, success: false})
+    return
+  }
+
+  try {
+    const result = await prisma.category.create({data: value})
+    res.status(201).json({data: result, success: true})
+  } catch (e) {
+    res.status(400).json({error: e, success: false})
+  }
+}
