fd7ced10990efa1c6df595d3d77048eec23376b0
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:27:42 +0300

    chore: add tag index api

---
+++ pages/api/tags/index.ts
@@ -0,0 +1,18 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import prisma from '../../../utils/prisma'
+
+export default async function LoadTags(req: NextApiRequest, res: NextApiResponse) {
+
+  const {offset = 0, limit = 10} = req.body
+
+  try {
+    const result = await prisma.tag.findMany({
+      skip: offset,
+      take: limit,
+    })
+    res.status(200).json({data: result, success: true})
+  }
+  catch (e) {
+    res.status(500).json({error: e, success: false})
+  }
+}
