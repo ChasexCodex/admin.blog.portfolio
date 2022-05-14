827b80720a7e7e06b940a0f03767b0fa5142ddb0
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:27:59 +0300

    chore: add tag delete api

---
+++ pages/api/tags/delete.ts
@@ -0,0 +1,18 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import prisma from '../../../utils/prisma'
+
+export default async function UpdateTag(req: NextApiRequest, res: NextApiResponse) {
+  const {id} = req.body
+
+  if (!id) {
+    res.status(400).json({error: 'id is required', success: false})
+    return
+  }
+
+  try {
+    const result = await prisma.tag.delete({where: {id: parseInt(id)}})
+    res.status(200).json({data: result, success: true})
+  } catch (e) {
+    res.status(400).json({error: e, success: false})
+  }
+}
