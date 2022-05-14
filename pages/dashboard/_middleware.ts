25337ee284e913f8b91424a3ca6ee35a6e3ad365
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 21:04:29 +0300

    chore: middleware only on production

--- pages/dashboard/_middleware.ts
+++ pages/dashboard/_middleware.ts
@@ -2,6 +2,11 @@ import {NextRequest, NextResponse} from 'next/server'
 import {checkUser} from '../../utils/auth'
 
 export async function middleware(req: NextRequest) {
+
+  if (process.env.NODE_ENV === 'development') {
+    return NextResponse.next()
+  }
+
   const header = req.cookies['Authorization']
 
   if (checkUser(header)) {
