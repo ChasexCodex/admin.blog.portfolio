943fc4eecb39ab2776780e2dbbfb7f99f318ba9a
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:24:19 +0300

    chore: add category view api

---
+++ pages/api/categories/view.ts
@@ -0,0 +1,17 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import prisma from '../../../utils/prisma'
+
+export default async function UpdatePost(req: NextApiRequest, res: NextApiResponse) {
+  const {id} = req.body
+
+  if (!id) {
+    return res.status(400).json({message: 'id is required', success: false})
+  }
+
+  try {
+    const result = await prisma.post.findFirst({where: {id: parseInt(id)}})
+    res.status(201).json({data: result, success: true})
+  } catch (e) {
+    res.status(400).json({error: e, success: false})
+  }
+}
