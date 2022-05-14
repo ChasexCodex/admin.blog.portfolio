7f8804276122017c8e73430329aef3480bf22d5e
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:24:04 +0300

    chore: add category index api

---
+++ pages/api/categories/index.ts
@@ -0,0 +1,18 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import prisma from '../../../utils/prisma'
+
+export default async function LoadCategories(req: NextApiRequest, res: NextApiResponse) {
+
+  const {offset = 0, limit = 10} = req.body
+
+  try {
+    const result = await prisma.category.findMany({
+      skip: offset,
+      take: limit,
+    })
+    res.status(200).json({data: result, success: true})
+  }
+  catch (e) {
+    res.status(500).json({error: e, success: false})
+  }
+}
