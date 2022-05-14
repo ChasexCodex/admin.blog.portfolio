1ce0f411d0f7eec0eea793b1737e9b40afffad32
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:27:51 +0300

    chore: add tag view api

---
+++ pages/api/tags/view.ts
@@ -0,0 +1,17 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import prisma from '../../../utils/prisma'
+
+export default async function UpdateTag(req: NextApiRequest, res: NextApiResponse) {
+  const {id} = req.body
+
+  if (!id) {
+    return res.status(400).json({message: 'id is required', success: false})
+  }
+
+  try {
+    const result = await prisma.tag.findFirst({where: {id: parseInt(id)}})
+    res.status(201).json({data: result, success: true})
+  } catch (e) {
+    res.status(400).json({error: e, success: false})
+  }
+}
