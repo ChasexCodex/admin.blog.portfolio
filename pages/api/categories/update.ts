a0f53a77f04240b99a6b21b6710c1215dbab2b2a
ElyasAmri <elyas.amri@gmail.com>
2022-05-14 22:24:26 +0300

    chore: add category update api

---
+++ pages/api/categories/update.ts
@@ -0,0 +1,28 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import Joi from 'joi'
+import prisma from '../../../utils/prisma'
+
+const UpdateCategorySchema = Joi
+    .object({
+      id: Joi.number().required(),
+      name: Joi.string().required(),
+    })
+
+export default async function UpdatePost(req: NextApiRequest, res: NextApiResponse) {
+  const {value: {id, ...value}, error} = UpdateCategorySchema.validate(req.body)
+
+  if (error) {
+    res.status(400).json({error, success: false})
+    return
+  }
+
+  try {
+    const result = prisma.post.update({
+      where: {id: parseInt(id)},
+      data: value,
+    })
+    res.status(201).json({data: result, success: true})
+  } catch (e) {
+    res.status(400).json({error: e, success: false})
+  }
+}
