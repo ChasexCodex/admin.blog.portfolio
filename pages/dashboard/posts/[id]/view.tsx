d9eea570a3679cb6a32ef3e9712c2da702e7cc7c
ElyasAmri <elyas.amri@gmail.com>
2022-05-17 15:16:04 +0300

    design: post view

--- pages/dashboard/posts/[id]/view.tsx
+++ pages/dashboard/posts/[id]/view.tsx
@@ -10,7 +10,8 @@ import rehypeCodeTitles from 'rehype-code-titles'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import {prisma} from '@/prisma'
-import Link from 'next/link'
+import {Link} from '@/components'
+import {ReactNode, useState} from 'react'
 
 export const getServerSideProps: GetServerSideProps<any, {id: string}> = async ({params}) => {
   if (!params) {
@@ -59,23 +60,67 @@ type Props = {
   source: MDXRemoteSerializeResult
 }
 
+type Tab = 'info' | 'view'
+
+const Row = ({name, value}: {name: string, value: ReactNode}) => {
+  return (
+      <tr className="odd:bg-gray-200 h-14">
+        <td>{name}</td>
+        <td>{value}</td>
+      </tr>
+  )
+}
+
 const ViewPost = ({post, source}: Props) => {
 
+  const [tab, setTab] = useState<Tab>('info')
+  const changeTab = (tab: Tab) => () => setTab(tab)
   const components = {}
 
   return (
-      <div>
-        <Link href="/dashboard/posts"><a>Back</a></Link>
+      <div className="p-4">
+        <Link href="/dashboard/posts" className="btn bg-black text-white max-w-max">Back</Link>
 
-        <p>{post.id}</p>
-        <p>{post.title}</p>
-        <p>{post.slug}</p>
-        <MDXRemote {...source} components={components}/>
-        <p>{post.created_at}</p>
-        <p>{post.updated_at}</p>
-        <p>{post.author}</p>
-        <p>{post.category.name}</p>
-        <p>{post.tags.map(t => <span key={t.id}>{t.name},</span>)}</p>
+        {tab === 'info' &&
+          <table className="table-auto w-full text-center shadow my-4">
+            <tbody>
+            <Row name="ID" value={post.id}/>
+            <Row name="Title" value={post.title}/>
+            <Row name="Slug" value={post.slug}/>
+            <Row name="Author" value={post.author}/>
+            <Row name="Published" value={post.published.toString()}/>
+            <Row name="Category" value={
+              <span className="pill bg-orange-400 text-sm">
+                {post.category.name}
+              </span>
+            }/>
+            <Row name="Tags" value={
+              <div className="space-x-1">
+                {post.tags.map(t =>
+                    <span key={t.id} className="tag">
+                      {t.name}
+                    </span>,
+                )}
+              </div>
+            }/>
+            <Row name="Created At" value={post.created_at}/>
+            <Row name="Updated At" value={post.updated_at}/>
+            </tbody>
+          </table>
+        }
+
+        {tab === 'view' &&
+          <div className="flex-pass-col my-4">
+            <article className="flex-1 p-4 markdown-body">
+              {source && <MDXRemote {...source} components={components}/>}
+            </article>
+          </div>
+        }
+
+        <div className="flex flex-row space-x-2 mt-8">
+          <button onClick={changeTab('info')} className="btn bg-orange-500">Info</button>
+          <button onClick={changeTab('view')} className="btn bg-orange-500">View</button>
+        </div>
       </div>
   )
 }
