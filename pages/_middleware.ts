c659ed25d17f551d8ca3cccb88d17649e7a86bc8
ElyasAmri <elyas.amri@gmail.com>
2022-05-26 16:32:26 +0300

    code: middleware behaves on env

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,14 +3,21 @@ import {checkUser} from '@/utils/auth'
 
 export async function middleware(req: NextRequest) {
 
-  if (process.env.NODE_ENV === 'development') {
+  const next = () => {
+    if (req.nextUrl.pathname === '/') {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
+    }
     return NextResponse.next()
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    return next()
   }
 
   const header = req.cookies['Authorization']
 
   if (checkUser(header)) {
-    return NextResponse.next()
+    return next()
   }
 
   return new Response('Auth required', {
