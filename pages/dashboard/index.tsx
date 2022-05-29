577eb03a9ed0e41eef1aabdbeec5c6661d87da9d
ElyasAmri <elyas.amri@gmail.com>
2022-05-29 18:35:50 +0300

    lint: dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,11 @@
-const Dashboard = () => {
-	return (
-		<div className="flex justify-center items-center flex-1 flex flex-col">
-			<div className="w-full xl:max-w-6xl flex-1 my-4 p-4">
-				<p className="text-center italic text-3xl font-semibold">
-					Dashboard
-				</p>
-			</div>
+const Dashboard = () => (
+	<div className="flex justify-center items-center flex-1 flex flex-col">
+		<div className="w-full xl:max-w-6xl flex-1 my-4 p-4">
+			<p className="text-center italic text-3xl font-semibold">
+				Dashboard
+			</p>
 		</div>
-	)
-}
+	</div>
+)
 
 export default Dashboard
