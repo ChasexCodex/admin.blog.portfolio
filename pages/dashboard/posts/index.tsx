2cb5534f0bb19b079e2dadf86814c8a5b1111037
ElyasAmri <elyas.amri@gmail.com>
2022-05-31 14:07:04 +0300

    code & ui & lint: couple post row with post index + fix delete function for posts

--- pages/dashboard/posts/index.tsx
+++ pages/dashboard/posts/index.tsx
@@ -1,8 +1,8 @@
 import {GetServerSideProps} from 'next'
-import {PostRow, Link} from '@/components'
+import {Link} from '@/components'
 import {prisma} from '@/prisma'
 import {Post} from '@/types'
-import {convertTimestampToMoment} from '@/utils'
+import {convertTimestampToMoment, http} from '@/utils'
 
 const perPage = 10
 
@@ -23,6 +23,56 @@ export const getServerSideProps: GetServerSideProps = async ({query}) => {
 	}
 }
 
+const deletePost = (id: number) => () => {
+	http.post('/api/posts/delete', {id})
+		.then(() => {
+			window.location.reload()
+		})
+		.catch(err => {
+			console.log(err)
+			// TODO: display errors
+		})
+}
+
+const revalidate = (id: number) => () => {
+	http.post(`/api/posts/revalidate`, {id})
+		.then(() => {
+			// TODO: display success
+		})
+		.catch(err => {
+			console.log(err)
+			// TODO: display errors
+		})
+}
+
+type RowProps = {
+	post: Post
+}
+
+const PostRow = ({post}: RowProps) => (
+	<tr key={post.id}
+			className="border-t even:bg-gray-200 h-14
+								 dark:bg-neutral-500 dark:even:bg-neutral-600 dark:border-gray-500
+								 ">
+		<td className="hidden xl:table-cell">{post.id}</td>
+		<td>{post.title}</td>
+		<td className="hidden xl:table-cell">{post.slug}</td>
+		<td className="hidden xl:table-cell">{post.author}</td>
+		<td className="hidden xl:table-cell">{post.created_at}</td>
+		<td className="hidden xl:table-cell">{post.updated_at}</td>
+		<td>
+			<div className="flex flex-col center space-y-0.5 text-black
+											xl:flex-row xl:space-x-0.5 xl:space-y-0
+											">
+				<Link href={`/dashboard/posts/${post.id}/edit`} className="btn w-full bg-yellow-400">Edit</Link>
+				<Link href={`/dashboard/posts/${post.id}/view`} className="btn w-full bg-blue-500">View</Link>
+				<button onClick={deletePost(post.id)} className="btn w-full bg-red-600">Delete</button>
+				<button onClick={revalidate(post.id)} className="btn w-full bg-white">Revalidate</button>
+			</div>
+		</td>
+	</tr>
+)
+
 type Props = {
 	posts: Post[]
 }
@@ -36,12 +86,12 @@ const IndexPost = ({posts}: Props) => (
 		{!!posts.length &&
 			<div className="shadow overflow-hidden">
 				<table className="table-auto w-full text-center
-			 									dark:text-white
-			 									">
+			 										dark:text-white
+			 										">
 					<thead>
 					<tr className="h-10 bg-gray-400
-											 dark:bg-gray-800
-											 ">
+												 dark:bg-gray-800
+												 ">
 						<th className="w-8 hidden xl:table-cell">#</th>
 						<th>Title</th>
 						<th className="hidden xl:table-cell">Slug</th>
