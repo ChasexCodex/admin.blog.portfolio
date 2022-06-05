b4cc92cd59de46e4271b2a19322ade8d3dc572fa
ElyasAmri <elyas.amri@gmail.com>
2022-06-05 17:36:42 +0300

    fix: use validation before delete

--- pages/api/posts/delete.ts
+++ pages/api/posts/delete.ts
@@ -1,16 +1,17 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {prisma} from '@/prisma'
+import {FindSchema} from '@/schemas'
 
 export default async function UpdatePost(req: NextApiRequest, res: NextApiResponse) {
-	const {id} = req.body
+	const {value, error} = FindSchema.validate(req.body)
 
-	if (!id || typeof id !== 'string') {
-		res.status(400).json({message: 'id is required', success: false})
+	if (error) {
+		res.status(400).json({error, success: false})
 		return
 	}
 
 	try {
-		const result = await prisma.post.delete({where: {id: parseInt(id)}})
+		const result = await prisma.post.delete({where: {id: value.id}})
 		res.status(201).json({data: result, success: true})
 	} catch (e) {
 		res.status(400).json({error: e, success: false})
