2934e4b4e52591fb66e5843029304ad080a73673
ElyasAmri <elyas.amri@gmail.com>
2022-06-06 07:54:15 +0300

    code: use find schema in delete api

--- pages/api/tags/delete.ts
+++ pages/api/tags/delete.ts
@@ -1,16 +1,17 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {prisma} from '@/prisma'
+import {FindSchema} from '@/schemas'
 
 export default async function UpdateTag(req: NextApiRequest, res: NextApiResponse) {
-	const {id} = req.body
+	const {value, error} = FindSchema.validate(req.body)
 
-	if (!id) {
-		res.status(400).json({error: 'id is required', success: false})
+	if (error) {
+		res.status(400).json({error, success: false})
 		return
 	}
 
 	try {
-		const result = await prisma.tag.delete({where: {id}})
+		const result = await prisma.tag.delete({where: {id: value.id}})
 		res.status(200).json({data: result, success: true})
 	} catch (e) {
 		res.status(400).json({error: e, success: false})
